ee13d46e2341e767a3407a2fc23a8d2ccaabb3af
Carlos Villavicencio <carlos.po5i@gmail.com>
2019-02-04 20:39:38 -0500

    Fix identation

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def h1(text: str) -> str:
-  return f'<h1>{text}<h1>'
+    return f'<h1>{text}<h1>'
 
 
 if __name__ == "__main__":
-    # something: int = 5  # Bad
-    something: str = 'Something'  # Good
+    something: int = 5
 
     something_as_title: str = h1(something)
 
